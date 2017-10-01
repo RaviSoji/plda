18e1796eb7d789bd9a1ca84d1d13817529782b2a
RaviSoji <raviisojitraa@gmail.com>
2017-10-01 14:08:22 -0400

    Worked a bit more on cross validation code.

--- cross_validation/cross_validation.py
+++ cross_validation/cross_validation.py
@@ -20,26 +20,48 @@ from scipy.misc import imresize
 from sklearn.decomposition import PCA
 
 
-def load_imgs_and_labels(directory, as_grey=True):
+def load_jpgs_and_lbls(directory, dataset, as_grey=True):
     """ Reads '.jpg' files from a directory as grayscaled images.
 
-     DESCRIPTION
-     Grayscaling weights: 0.2125 RED + 0.7154 GREEN + 0.0721 BLUE
+    DESCRIPTION
+     Grayscaling weights: 0.2125 RED + 0.7154 GREEN + 0.0721 BLUE.
+     cafe and google data file names should begin with the emotion name.
 
     ARGUMENT
-    directory  (string): Directory containing the files to be read.
+     directory    (str): Directory containing the files to be read.
+     dataset      (str): Must be set to either 'cafe' or 'google'.
 
-    RETURN
-    images      (list): List of 2D ndarrays (grayscaled images).
+    RETURNS
+     images      (list): List of 2D ndarrays (grayscaled images).
+     labels      (list): List of strings in the same order as images.
     """
+    assert isinstance(directory, str)
+    assert dataset == 'cafe' or dataset == 'google'
+    assert as_grey == True
+
     images = []
     labels = []
     for fname in os.listdir(directory):
         if fname.endswith('.jpg'):
             img = imread(directory + '/' + fname, as_grey=as_grey)
             images.append(img)
-            labels.append(fname[:3])
+            label = get_label(fname, dataset=dataset)
+            labels.append(label)
+
     return images, labels
+
+def get_label(filename, dataset):
+    if dataset == 'cafe':
+        return filename[:3]
+
+    elif dataset == 'google':
+        label = ''.join([i for i in filename if not i.isdigit()])
+        label = label[:-4]
+
+        return label
+
+    else:
+        raise ValueError('dataset must be set to either \"cafe\" or \"google\".')
 
 def get_smallest_shape(images):
     smallest_size = -1
@@ -77,7 +99,7 @@ def get_principal_components(flattened_images, n_components='default',
 
     return principal_components
 
-def preprocess_cafe_faces(images, n_components='default', resize_shape='default'):
+def preprocess_faces(images, n_components='default', resize_shape='default'):
     """ Notes: Images are all square, but not the same size. We resize all the
                 images to be the same sized square, thereby preserving the
                 aspect ratios.
@@ -93,17 +115,81 @@ def preprocess_cafe_faces(images, n_components='default', resize_shape='default'
         prepped_img = imresize(img, resize_shape).astype(float)
         prepped_img = prepped_img.flatten()
         preprocessed_images.append(prepped_img)
-    preprocessed_images = get_principal_components(preprocessed_images, n_components)
+    preprocessed = get_principal_components(preprocessed_images, n_components)
 
-    return preprocessed_images
+    return preprocessed
 
-def preprocess_google_faces(images, reszie_shape='default'):
+def leave_n_out(X, Y, n, max_runs='default'):
+    """ n is the number to leave out for the test set(s). 
+        Note that this code uses set subtraction to get the training indices,
+        so this will mean that ordering of the training sets will be similar.
     """
-    """
-    raise NotImplementedError
+    if max_runs == 'default':
+        max_runs = X.shape[0] - n
+
+    idxs = np.arange(0, X.shape[0])
+    np.random.shuffle(idxs)
+
+    for x in range(X.shape[0] - n):
+        if x < max_runs:
+            test_idxs = idxs[x: x + n]
+            training_idxs = set(idxs) - set(test_idxs)
+            training_idxs = np.asarray(list(training_idxs))
+    
+            train_X = X[training_idxs, :]
+            train_Y = Y[training_idxs]
+            test_X = X[test_idxs, :]
+            test_Y = Y[test_idxs]
+    
+            yield (train_X, train_Y), (test_X, test_Y)
+
+        else:
+            print('Warning: max_runs was not set to default, so estimate of
+                   performance will be more biased.')
+            break
+
+def predict_leave_n_out(X, Y, n=10, MAP_estimate=True, max_runs='default'):
+    predicted_ys = []
+    actual_ys = []
+    for (train_x, train_y), (test_x, test_y) in leave_n_out(X, Y, n):
+        training_data = [(x, y) for (x, y) zip(train_X, train_Y)]
+        model = PLDA(training_data)
+
+        predictions = model.predict_class(test_X, MAP_estimate=MAP_estimate)
+
+        predicted_ys += [y for y in predictions]
+        actual_ys += [y for y in test_y]
+
+    return predicted_ys, actual_ys
+
+def get_scores(actual_Y, predicted_Y, return_confusion_matrix=True):
+    assert len(actual_Y) == len(predicted_Y)
+    classes = set(actual_Y)
+    n_total = len(actualY)
+    model_predictions = {y: 0 for y in classes}
+    matrix = {y: model_predictions for y in classes}}
+
+    for actual, predicted in zip(actual_Y, predicted_Y):
+        matrix[actual][predicted] += 1
+
+    score = 0
+    for actual in matrix.keys():
+        number_correct = matrix[actual][actual]
+        score += number_correct
+    score /= n_total
+
+    if return_confusion_matrix is False:
+        return score
+    else:
+        return score, confusion_matrix
+    
+#def cross_validate(X, Y, return_class_scores):
+#    fit
+#    predict
+#    return scores
 
 def main():
     img_dir = ''
-    X, Y = load_imgs_and_labels(img_dir, as_grey=True)
-    X = preprocess_cafe_faces(X)
-    
+    X, Y = load_jpgs_and_lbls(img_dir, as_grey=True)
+    X = preprocess_faces(X)
+
