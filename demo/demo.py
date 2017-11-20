8357183e50ce0c59a20b2e2bfa54112b654aeb71
RaviSoji <raviisojitraa@gmail.com>
2017-11-20 09:30:53 -0500

    Updated demo code and ipython notebook to reflect new changes to code.

--- demo/demo.py
+++ demo/demo.py
@@ -15,82 +15,21 @@
 
 import os
 import sys
-sys.path.append(os.getcwd() + '/../')
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+sys.path.append(os.getcwd() + '/../')
+from cross_validation.classification import PLDAClassification
 from matplotlib.patches import Ellipse
-from numpy.random import randint
 from numpy.random import multivariate_normal as m_normal
-from PLDA import PLDA
+from plda import PLDA
 from scipy.stats import norm, chi2
 
-
-def gen_artificial_data(n_classes, n_list, n_dims):
-    """ Generates a data set from multi-variate Gaussians. """
-    assert len(n_list) == n_classes
-    np.random.seed(1)  # Ensures that the same plot is generated every time.
-
-    # Generate the within and between class covariance matrices.
-    w_class_cov = np.random.randint(-10, 10, n_dims ** 2)
-    w_class_cov = w_class_cov.reshape(n_dims, n_dims)
-    w_class_cov = np.matmul(w_class_cov, w_class_cov.T)  # Make symmetric.
-    
-    bw_class_cov = randint(100, 10000, n_dims)
-    bw_class_cov = np.diag(bw_class_cov)
-     # LDA assumes within class covariance to be the same for all clusters,
-     #  so do not generate a new one for each mean, unless you want to see
-     #  how the model performs when the data does not meet LDA assumptions.
-
-    # Generate cluster means.
-    means = m_normal(np.zeros(n_dims), bw_class_cov, n_classes)
-
-    # Generate points from each of the clusters.
-    points = []
-    for x in range(len(n_list)):
-        points.append(m_normal(means[x, :], w_class_cov, n_list[x]))
-    points = np.vstack(points)
-
-    # Generate labels.
-    labels = []
-    for x in range(len(means)):
-        labels += [x] * n_list[x]
-
-    return points, labels
-
-def plot_test_classifications(PLDA_model, ax, MAP_estimate=True):
-    n_test = 50000  # Number of test data.
-
-    data = np.array([[x, y] for ((x, y), label) in PLDA_model.raw_data])
-    std = np.std(data, axis=0)
-    x = data[:, 0]
-    y = data[:, 1]
-    x_min, x_max = np.amin(x) - .25 * std[0], np.amax(x) + .25 * std[0]
-    y_min, y_max = np.amin(y) - .25 * std[1], np.amax(y) + .25 * std[1]
-
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
-
-    n_classes = len(PLDA_model.stats)
-    colors = cm.rainbow(np.linspace(0, 1, n_classes))
-
-    test_x = np.random.randint(x_min, x_max, n_test)
-    test_y = np.random.randint(y_min, y_max, n_test)
-    classifications = PLDA_model.predict_class(np.array([test_x, test_y]).T,
-                                               MAP_estimate=MAP_estimate)
-
-    idxs = np.argsort(np.array(classifications))
-    classifications = np.array(classifications)[idxs]
-    test_x = test_x[idxs]
-    test_y = test_y[idxs]
-    ax.scatter(test_x, test_y, color=colors[classifications], s=.5)
-
-    return ax
 
 def cov_ellipse(cov, q=None, nsig=None, **kwargs):
     """ Code is slightly modified, but essentially borrowed from: 
          https://stackoverflow.com/questions/18764814/make-contour-of-scatter
-    """ 
+    """
     if q is not None:
         q = np.asarray(q)
     elif nsig is not None:
@@ -105,46 +44,91 @@ def cov_ellipse(cov, q=None, nsig=None, **kwargs):
 
     return width, height, rotation
 
-def plot_training_contours(PLDA_model, ax, nsig):
-    """ Plots contour of the 95% CI of each multivariate Gaussian in the model.
-    """
-    ells = []
-    for label in PLDA_model.stats.keys():
-        mean = PLDA_model.stats[label]['μ']
-        cov = PLDA_model.stats[label]['covariance']
-        w, h, t = cov_ellipse(cov, nsig=2)
-        ells.append(Ellipse(mean, width=w, height=h, angle=t))
+def plot_scatter(ax, x, y, s=5, c='black', label='', plot_training_cov=False,
+                 model=None):
+    assert len(x.shape) == 1
+    assert len(y.shape) == 1
+    if plot_training_cov is True:
+        assert model is not None
 
-    for e in ells: 
-        ax.add_artist(e)
-        e.set_facecolor('none')
+    ax.scatter(x, y, c=c, s=s, label=label)
+
+    if plot_training_cov is True:
+        cov = model.data[label]['cov']
+        mean_x, mean_y = model.data[label]['mean']
+        w, h, deg = cov_ellipse(cov, nsig=2)
+        ell = Ellipse(xy=(mean_x, mean_y),
+                      width=w, height=h,
+                      angle=deg, linewidth=2)
+        ell.set_facecolor('none')
+        ell.set_edgecolor('black')
+        ax.add_patch(ell)
+    ax.set_aspect('equal')
 
     return ax
 
+def lbls_to_clrs(lbls, lbl_clr_pairs):
+    assert len(np.unique(lbls)) == len(lbl_clr_pairs)
+    assert isinstance(lbls, np.ndarray)
+    assert isinstance(lbl_clr_pairs, list)
+    assert isinstance(lbl_clr_pairs[0], tuple)
+
+    colors = np.empty((*lbls.shape, lbl_clr_pairs[0][1].shape[0]),
+                      dtype=lbl_clr_pairs[0][1].dtype)
+    for lbl, clr in lbl_clr_pairs:
+        colors[lbls == lbl] = clr
+
+    return colors
+
+
+def gen_training_set(n_gaussians, sample_size, n_dims):
+    cov = np.random.randint(-10, 10, (n_dims, n_dims))
+    cov = np.matmul(cov, cov.T) + np.eye(n_dims) * np.random.rand(n_dims)
+
+    pts = np.vstack([m_normal(np.ones(n_dims) * np.random.randint(-100, 100, 1),
+                              cov, sample_size) \
+                     for x in range(n_gaussians)])
+    lbls = np.hstack([['gaussian_{}'.format(x)] * sample_size for x in range(n_gaussians)])
+
+    return pts, lbls
+
 def main():
-    n_classes = 10  # Number of clusters (i.e. multivariate Gaussians)
-    n_list = [300 * (x % 3 + 1) for x in range(1, n_classes + 1)]  # Sample size
-    n_dims = 2  # Dimensionality of the data.
+    n_gaussians = 5
+    sample_size = 100
+    n_dims = 2
+    n_test = 5000
 
-    points, labels = gen_artificial_data(n_classes, n_list, n_dims)
+    # Initialize training data, task, and model.
+    np.random.seed(0)
+    train_X, train_Y = gen_training_set(n_gaussians, sample_size, n_dims)
+    task = PLDAClassification(train_X, train_Y)
+    model = PLDA(train_X, train_Y)
 
-    data = []
-    for x, y in zip(points, labels):
-        data.append((x, y))
+    # Generate test data.
+    margin = np.sqrt(np.cov(train_X.T).diagonal().sum()) * .1
+    (min_x, min_y), (max_x, max_y) = np.min(train_X, axis=0) - margin,\
+                                     np.max(train_X, axis=0) + margin
+    test = np.asarray([np.random.uniform(min_x, max_x, n_test),
+                       np.random.uniform(min_y, max_y, n_test)]).T
 
-    model = PLDA(data, save_raw=True)
-
-    fig = plt.figure(0)
-    ax = fig.add_subplot(111)
-    ax = plot_training_contours(model, ax, nsig=2)
-    plot_test_classifications(model, ax, MAP_estimate=False)
-    plt.show()
+    # Classify test data using the plda model.
+    predictions, log_probs = task.predict(test, model=model,
+                                          standardize_data=True)
+    colors = cm.rainbow(np.linspace(0, 1, n_gaussians))
+    unique = np.unique(predictions)
+    c = lbls_to_clrs(predictions, [pair for pair in zip(unique, colors)])
     
-    # Plots 95% CI level contours of Gaussians fit to the TRAINING data.
-    #  Colors of points represent model classifications. If the model
-    #  is accurate, points inside the counters should be colored the same
-    #  as long as there isn't much overlap between the distributions (i.e.
-    #  countours).
+    fig, ax_arr = plt.subplots(1)
+    for label in unique:
+        idxs = predictions == label
+        plot_scatter(ax_arr, test[idxs, 0], test[idxs, 1],
+                     label=label, c=c[idxs, :], plot_training_cov=True,
+                     model=model)
 
-if __name__ == '__main__':
-    main()
+    (min_x, min_y), (max_x, max_y) = np.min(test, axis=0), np.max(test, axis=0)    
+    ax_arr.set_xlim(min_x, max_x)
+    ax_arr.set_ylim(min_y, max_y)
+    fig.set_size_inches(10, 10)
+    plt.xlabel('Dimension 1', fontsize=12)
+    plt.ylabel('Dimension 2', fontsize=12)
+    plt.title('Demo', fontsize=20)
